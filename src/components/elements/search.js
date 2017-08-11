b0a709f41d286f6c2811cfee3a43f8880c7acafe
benbeatty85 <benbeatty85@gmail.com>
2017-08-11 15:16:44 -0400

    Finished CSS for elements

--- src/components/elements/search.js
+++ src/components/elements/search.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './search.css';
 // import api from '../../api';
 // import BoardCard from '../elements/BoardCard';
 // import CreateBoard from '../modals/CreateBoard';
